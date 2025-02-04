3c333c19b6f5452e9967e90868a82a9a7e19cd09
Kynou Dot Com <mccinstructor@gmail.com>
2025-02-03 19:52:50 -0500

    First commit

---
+++ Kynou/Helpers/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Kynou.Helpers
+{
+    public static class StringHelper
+    {
+        public static string GenerateTimestamp()
+        {
+            var d = DateTime.Now;
+
+            return $"{d.Year}_{d.Month}_{d.Day}_{d.Hour}_{d.Minute}_{d.Second}_{d.Millisecond}";
+        }
+    }
+}
---
+++ Kynou/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Kynou.Helpers;
+using System.Text;
+using System.Windows;
+
+namespace Kynou
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string WINDOW_TITLE = "Kynou | ";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetTitle("Welcome!");
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            WindowState = WindowState.Maximized;
+        }
+
+        private void SetTitle(string msg,bool? includeTimestamp = false)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(WINDOW_TITLE);
+
+            sb.Append(msg);
+
+            if (includeTimestamp == true)
+            {
+                string newTimestamp = StringHelper.GenerateTimestamp();
+
+                sb.Append(newTimestamp);
+            }
+
+            this.Dispatcher.Invoke(() =>
+            {
+                Title = sb.ToString();
+            });
+        }
+    }
+}
